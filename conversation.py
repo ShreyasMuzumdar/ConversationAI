f34f4a34568a9630c9cd3bac4f4585f43a4f7e3e
Shreyas Muzumdar <Shreyas2966@gmail.com>
2024-12-24 18:32:17 -0500

    cd

--- conversation.py
+++ conversation.py
@@ -4,7 +4,7 @@ from llama_cpp import Llama
 from TTS.api import TTS
 
 # Load Models
-llama = Llama(model_path="./llama-7b.ggmlv3.q4_0.bin")  # Path to LLaMA model
+llama = Llama(model_path="/Users/shreyas/ConversationAI/llama.cpp/src/llama.cpp") # Path to LLaMA model
 tts = TTS(model_name="tts_models/en/ljspeech/tacotron2-DDC")
 porcupine = Porcupine(
     access_key="+b/VvsTn8hkmOXbtwUqHcw6L+6DhoZzoc0og+pzqHrou7A5v6nY40w==", keyword_paths=["Hello-Joe_en_mac_v3_0_0.ppn"]
